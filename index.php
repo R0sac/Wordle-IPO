4dc21e1929b0cb248423a4d76fec02b2ddc6d2c5
super <poltelljove@gmail.com>
2022-10-18 17:08:01 +0200

    funcionalitat tecles realitzada

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function getRandomLine($filename) { 
-        $lines = file($filename); 
-        return $lines[array_rand($lines)]; 
-    }
-    $randomWord = getRandomLine("catala_5.txt");
-?>
