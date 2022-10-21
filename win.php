70925434eb7f012e83eb1ec43ebb5f2e2af37a77
Iker Gonzalez <igonzalezvirsa@gmail.com>
2022-10-21 16:13:24 +0200

    Nom fitxers canviats i CSS en un fitxer

---
+++ win.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Victoria Wordle</title>
+</head>
+<body>
+    <div id="contPrincipal">
+        <div id="contDreta">
+            <img src="wordleBanner.png" id="imgDreta">
+        </div>
+        <div id="contEsquerra">
+            <img src="wordleBanner.png" id="imgEsquerra">
+        </div>
+        <div id="contCentre">
+            <h1 id="titol">WORDLE</h1>
+            <img src="wordleVictoria.gif" id="imgCentre">
+            <h1 id="textFinalPartida">HAS GUANYAT!</h1>
+        </div>
+    </div>
+</body>
+</html>
