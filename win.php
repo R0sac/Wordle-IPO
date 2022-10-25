876d1189639521cf27bcdf9575d066ceef82e582
Iker Gonzalez <igonzalezvirsa@gmail.com>
2022-10-25 18:20:34 +0200

    Responsive

--- win.php
+++ win.php
@@ -32,43 +32,41 @@
                 <h1 id="textFinalPartida">HAS GUANYAT!</h1>
             </div>
             <div id="contTeclat">
-            <table  class="tablaTeclado">
-                    <tr>
-                        <td><button onclick="addLetter('Q')" id="Q" class="boton_personalizado">Q</button></td>
-                        <td><button onclick="addLetter('W')" id="W" class="boton_personalizado">W</button></td>
-                        <td><button onclick="addLetter('E')" id="E" class="boton_personalizado">E</button></td>
-                        <td><button onclick="addLetter('R')" id="R" class="boton_personalizado">R</button></td>
-                        <td><button onclick="addLetter('T')" id="T" class="boton_personalizado">T</button></td>
-                        <td><button onclick="addLetter('Y')" id="Y" class="boton_personalizado">Y</button></td>
-                        <td><button onclick="addLetter('U')" id="U" class="boton_personalizado">U</button></td>
-                        <td><button onclick="addLetter('I')" id="I" class="boton_personalizado">I</button></td>
-                        <td><button onclick="addLetter('O')" id="O" class="boton_personalizado">O</button></td>
-                        <td><button onclick="addLetter('P')" id="P" class="boton_personalizado">P</button></td>
-                    </tr>
-                    <tr>
-                        <td><button onclick="addLetter('A')" id="A"class="boton_personalizado">A</button></td>
-                        <td><button onclick="addLetter('S')" id="S"class="boton_personalizado">S</button></td>
-                        <td><button onclick="addLetter('D')" id="D"class="boton_personalizado">D</button></td>
-                        <td><button onclick="addLetter('F')" id="F"class="boton_personalizado">F</button></td>
-                        <td><button onclick="addLetter('G')" id="G"class="boton_personalizado">G</button></td>
-                        <td><button onclick="addLetter('H')" id="H"class="boton_personalizado">H</button></td>
-                        <td><button onclick="addLetter('J')" id="J"class="boton_personalizado">J</button></td>
-                        <td><button onclick="addLetter('K')" id="K"class="boton_personalizado">K</button></td>
-                        <td><button onclick="addLetter('L')" id="L"class="boton_personalizado">L</button></td>
-                        <td><button onclick="addLetter('Ç')" id="Ç"class="boton_personalizado">Ç</button></td>
-                    </tr>
-                    <tr>
-                        <td><button onclick="jumpLine()" id="enviar" class="boton_personalizado">ENVIAR</button></td>
-                        <td><button onclick="addLetter('Z')" id="Z" class="boton_personalizado">Z</button></td>
-                        <td><button onclick="addLetter('X')" id="X" class="boton_personalizado">X</button></td>
-                        <td><button onclick="addLetter('C')" id="C" class="boton_personalizado">C</button></td>
-                        <td><button onclick="addLetter('V')" id="V" class="boton_personalizado">V</button></td>
-                        <td><button onclick="addLetter('B')" id="B" class="boton_personalizado">B</button></td>
-                        <td><button onclick="addLetter('N')" id="N" class="boton_personalizado">N</button></td>
-                        <td><button onclick="addLetter('M')" id="M" class="boton_personalizado">M</button></td>
-                        <td colspan="2"><button onclick="deleteLetter()" id="borrar" class="boton_personalizado">BORRAR</button></td>
-                    </tr>
-                </table>
+                <div id="teclatFila1">
+                    <button onclick="addLetter('Q')" id="Q" class="boton_personalizado">Q</button>
+                    <button onclick="addLetter('W')" id="W" class="boton_personalizado">W</button>
+                    <button onclick="addLetter('E')" id="E" class="boton_personalizado">E</button>
+                    <button onclick="addLetter('R')" id="R" class="boton_personalizado">R</button>
+                    <button onclick="addLetter('T')" id="T" class="boton_personalizado">T</button>
+                    <button onclick="addLetter('Y')" id="Y" class="boton_personalizado">Y</button>
+                    <button onclick="addLetter('U')" id="U" class="boton_personalizado">U</button>
+                    <button onclick="addLetter('I')" id="I" class="boton_personalizado">I</button>
+                    <button onclick="addLetter('O')" id="O" class="boton_personalizado">O</button>
+                    <button onclick="addLetter('P')" id="P" class="boton_personalizado">P</button>
+                </div>
+                <div id="teclatFila2">
+                    <button onclick="addLetter('A')" id="A"class="boton_personalizado">A</button>
+                    <button onclick="addLetter('S')" id="S"class="boton_personalizado">S</button>
+                    <button onclick="addLetter('D')" id="D"class="boton_personalizado">D</button>
+                    <button onclick="addLetter('F')" id="F"class="boton_personalizado">F</button>
+                    <button onclick="addLetter('G')" id="G"class="boton_personalizado">G</button>
+                    <button onclick="addLetter('H')" id="H"class="boton_personalizado">H</button>
+                    <button onclick="addLetter('J')" id="J"class="boton_personalizado">J</button>
+                    <button onclick="addLetter('K')" id="K"class="boton_personalizado">K</button>
+                    <button onclick="addLetter('L')" id="L"class="boton_personalizado">L</button>
+                    <button onclick="addLetter('Ç')" id="Ç"class="boton_personalizado">Ç</button>
+                </div>
+                <div id="teclatFila3">
+                    <button onclick="jumpLine()" id="enviar" class="boton_personalizado_accio">ENVIAR</button>
+                    <button onclick="addLetter('Z')" id="Z" class="boton_personalizado">Z</button>
+                    <button onclick="addLetter('X')" id="X" class="boton_personalizado">X</button>
+                    <button onclick="addLetter('C')" id="C" class="boton_personalizado">C</button>
+                    <button onclick="addLetter('V')" id="V" class="boton_personalizado">V</button>
+                    <button onclick="addLetter('B')" id="B" class="boton_personalizado">B</button>
+                    <button onclick="addLetter('N')" id="N" class="boton_personalizado">N</button>
+                    <button onclick="addLetter('M')" id="M" class="boton_personalizado">M</button>
+                    <button onclick="deleteLetter()" id="borrar" class="boton_personalizado_accio">BORRAR</button>
+                </div>
             </div>
         </div>
     </div>
